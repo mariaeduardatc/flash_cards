49f634580837a38f078d4023b5a7767f64912c9b
mariaeduardatc <81043486+mariaeduardatc@users.noreply.github.com>
2022-10-03 10:14:17 +0800

    final adjustments

--- flash_card/flash_card/ViewController.swift
+++ flash_card/flash_card/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
     }
     
     func updateFlashcard(question: String, answer: String){
-        
+        frontLabel.text = question
+        backLabel.text = answer
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
